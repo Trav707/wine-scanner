542c7415e9307ae03ebfdd21e80ddc1bebb7cf8f
Travis Palmer <tpalmer707@gmail.com>
2026-03-25 19:33:04 -0700

    Fix safe area insets with !important to override Tailwind CDN cascade

--- public/sw.js
+++ public/sw.js
@@ -9,7 +9,7 @@
  * Bump VERSION when deploying a new build so old caches are pruned.
  */
 
-const VERSION   = 'v6'
+const VERSION   = 'v7'
 const CACHE     = `wine-scanner-${VERSION}`
 const SHELL_URL = '/'
 
