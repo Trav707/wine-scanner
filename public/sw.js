8f95bab570ddcd25204e4c62ecc3a67d51570bd7
Travis Palmer <tpalmer707@gmail.com>
2026-03-25 19:25:54 -0700

    Fix iPhone safe area: notch top padding and tab bar bottom clearance

--- public/sw.js
+++ public/sw.js
@@ -9,7 +9,7 @@
  * Bump VERSION when deploying a new build so old caches are pruned.
  */
 
-const VERSION   = 'v5'
+const VERSION   = 'v6'
 const CACHE     = `wine-scanner-${VERSION}`
 const SHELL_URL = '/'
 
